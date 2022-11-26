ab3bf0955260589dff170bfa219c31f4c7b18da3
thevortex <james.g.bowley@gmail.com>
2022-11-26 16:40:08 -0700

    script renaming, worldgen additions

---
+++ kubejs/startup_scripts/worldgen.js
@@ -0,0 +1,17 @@
+onEvent('worldgen.add', event => {
+  event.addOre((ore) => {
+    ore.id = "kubejs:mining_certus" // optional
+    ore.biomes = ['beyond_earth:moon_desert']
+    ore.addTarget('beyond_earth:moon_stone', 'ae2:quartz_ore')
+    ore.size(7)     // ores per cluster
+    ore.count(10,20)   // clusters per chunk, can also be a range eg ore.count(15,20)
+    ore.squared()
+    ore.triangleHeight(-34, 36)
+  })
+})
+  onEvent('worldgen.remove', event => {     //  WorldgenEvents.remove(event => {
+    event.removeSpawns(properties => {
+      properties.biomes = [ '#minecraft:overworld']
+      properties.mobs = [ 'thermal:basalz', 'thermal:blizz', 'thermal:blitz' ]
+    })
+  })
