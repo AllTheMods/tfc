5adaba521212b6e0a4838b5af386b55e7479ba4b
TheDyreBanana <thedyrebanana@gmail.com>
2022-12-10 02:16:53 -0500

    yes

---
+++ kubejs/client_scripts/beyond_earth/jei_removals.js
@@ -0,0 +1,7 @@
+onEvent('jei.hide.items', e => {
+    e.hide('beyond_earth:coal_torch')
+	e.hide('beyond_earth:coal_lantern')
+    e.hide('beyond_earth:compressor')
+    e.hide('beyond_earth:coal_generator')
+    e.hide('beyond_earth:hammer')
+})
